2c5720c960d84b892038ee049128ca33b59cd8de
Blake Gifford <blake@Blakes-MacBook-Pro.local>
2021-04-14 08:41:58 -0700

    new commit

--- django/great_number_game/app_one/views.py
+++ django/great_number_game/app_one/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 
 def index(request):
+    random.randint(1,100)
     return render(request, "index.html")
+
+
+def answer(request):
+    return 
 
 # Create your views here.

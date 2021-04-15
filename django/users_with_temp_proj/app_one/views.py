056bb0f8f2cd64405ffdcef60f4bc7db3650828a
Blake Gifford <blake@Blakes-MacBook-Pro.local>
2021-04-15 12:47:09 -0700

    4/15 commit

--- django/users_with_temp_proj/app_one/views.py
+++ django/users_with_temp_proj/app_one/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from app_one.models import Users
+
+def index(request):
+    context = {
+        "all_users" : Users.objects.all()
+    }
+    return render(request, "index.html", context)
+
+
+def create_user(request):
+    print(request.POST)
+
+    Users.objects.create(
+        first_name = request.POST['first_name'],
+        last_name = request.POST['last_name'],
+        email = request.POST['email'],
+        age = request.POST['age']
+    )
+    return redirect("/")
+
+
 
 # Create your views here.

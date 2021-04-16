b0c26b79c49fd503550aa199d0dad97b169e1eba
blake-gifford <blakegifford01@gmail.com>
2021-04-15 22:21:23 -0700

    new commit

--- django/dojos_and_ninjas_proj/app_one/views.py
+++ django/dojos_and_ninjas_proj/app_one/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, redirect
 
-# from app_one.models import Dojo
+from app_one.models import Dojo, Ninja
 
 def index(request):
-    # context = {
-    #     "all_users" : Users.objects.all()
-    # }
+    context = {
+        "all_dojos" : Dojo.objects.all(),
+        "all_ninjas" : Ninja.objects.all()
+    }
     return render(request, "index.html")
+
+
+def submit(request):
+    print(request.POST)
+
+    Dojo.objects.create(
+        first_name = request.POST['first_name'],
+        last_name = request.POST['last_name'],
+        dojo = request.POST['dojo']
+    )
+    return redirect('/')
 
 # Create your views here.

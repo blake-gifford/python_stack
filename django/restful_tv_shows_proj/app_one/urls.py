8b40cfcd94e630be56f8a1800ec0dcaaa2e577fd
blake-gifford <blakegifford01@gmail.com>
2021-04-19 22:56:32 -0700

    night commit 4/19

--- django/restful_tv_shows_proj/app_one/urls.py
+++ django/restful_tv_shows_proj/app_one/urls.py
@@ -5,5 +5,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('shows', views.all_shows),
+    path('shows/new', views.new_show),
+    path('shows/create', views.create_show),
+    path('shows/<int:show_id>', views.display_show),
+    path('shows/<int:show_id>/edit', views.edit_show),
+    path('shows/<int:show_id>/update', views.update_show),
+    path('shows/<int:show_id>/delete', views.delete_show)
 ]
 
--- django/restful_tv_shows_proj/app_one/views.py
+++ django/restful_tv_shows_proj/app_one/views.py
@@ -1,8 +1,49 @@
 from django.shortcuts import render, redirect
 
+from app_one.models import Show
 
 def index(request):
-    return render(request, 'index.html')
+    return redirect('/shows')
+
+
+def all_shows(request):
+    context = {
+        "all_shows" : Show.objects.all()
+    }
+    return render(request, 'all_shows.html', context)
+
+
+def new_show(request):
+    return render(request, "new_show.html")
+
+
+def create_show(request):
+    Show.objects.create(
+        title = request.POST['title'],
+        network = request.POST['network'],
+        release_date = request.POST['release_date'],
+        description = request.POST['description']
+    )
+    return redirect('/shows')
+
+
+def display_show(request, show_id):
+    context = {
+        'show': Show.objects.get(id = show_id)
+    }
+    return render(request, 'display.html', context)
+
+
+def edit_show(request, show_id):
+    pass
+
+
+def update_show(request, show_id):
+    pass
+
+
+def delete_show(request, show_id):
+    pass
 
 # Create your views here.
 

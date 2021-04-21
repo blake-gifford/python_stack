9f18eccd1d314ff6ebc31d22f9e330a5d5961737
blake-gifford <blakegifford01@gmail.com>
2021-04-20 20:24:53 -0700

    night commit

--- django/login_reg_proj/app_one/models.py
+++ django/login_reg_proj/app_one/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class UserI(models.Model):
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    email = models.CharField(max_length = 255)
+    password = models.CharField(max_length = 255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
 # Create your models here.
--- django/login_reg_proj/app_one/views.py
+++ django/login_reg_proj/app_one/views.py
@@ -1,7 +1,25 @@
+import bcrypt
+from django.contrib import messages
 from django.shortcuts import render, redirect
 
 
 def index(request):
+    return redirect('/dashboard')
+
+
+def register(request):
     return render(request, 'index.html')
+
+
+def login(request):
+    pass
+
+
+def logout(request):
+    pass
+
+
+def dashboard(request):
+    pass
 
 # Create your views here.

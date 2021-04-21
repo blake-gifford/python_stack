592693b8c32b69c3499292269de67144c1cef1e3
blake-gifford <blakegifford01@gmail.com>
2021-04-21 14:11:46 -0700

    4/21 commit

--- django/login_reg_proj/app_one/models.py
+++ django/login_reg_proj/app_one/models.py
@@ -3,47 +3,38 @@ from django.db import models
 import bcrypt
 import re
 
-
 class UserManager(models.Manager):
     def registration_validator(self, post_data):
         errors = {}
 
-# validate name data
-    if len(post_data['first_name']) < 1:
-        errors['first_name'] = "First name must be filled out."
-    if len(post_data['last_name']) < 1:
-        errors['last_name'] = "Last name must be filled out."
-
-
-# vaidate email
-    EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-    if not EMAIL_REGEX.match(post_data['email']):           
-        errors['email'] = "Invalid email address!"
-    else:
-        user_list = User.objects.filter(email = post_data['email'])
-        if len(user_list) > 0:
-            errors['email'] = "Email is already in use."
-
-
-    # validate passwords
-    if len(post_data['password']) < 8:
-            errors['password'] = "Password must be at least 8 characters."
-    if post_data['password'] != post_data['confirm_password']:
-        errors['confirm_password'] = "Password and Confirm password must match."
-
-
+        if len(post_data['first_name']) < 1:
+            errors['first_name'] = "First name must be filled out."
+        if len(post_data['last_name']) < 1:
+            errors['last_name'] = "Last name must be filled out."
     
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(post_data['email']):           
+            errors['email'] = "Invalid email address!"
+        else:
+            user_list = User.objects.filter(email = post_data['email'])
+            if len(user_list) > 0:
+                errors['email'] = "Email is already in use."
+        if len(post_data['password']) < 8:
+            errors['password'] = "Password must be at least 8 characters."
+        if post_data['password'] != post_data['confirm_password']:
+            errors['confirm_password'] = "Password and Confirm password must match."
+        return errors
+        
+
     def login_validator(self, post_data):
         errors = {}
-
         user_list = User.objects.filter(email = post_data['email'])
-        if len(user_list) > 0:
+        if len(user_list) > 1:
             user = user_list[0]
             if not bcrypt.checkpw(post_data['password'].encode(), user.password.encode()):
                 errors['password'] = "Invalid Login"
         else:
             errors['email'] = "Invalid Login"
-
         return errors
 
 
--- django/login_reg_proj/app_one/views.py
+++ django/login_reg_proj/app_one/views.py
@@ -2,25 +2,67 @@ import bcrypt
 from django.contrib import messages
 from django.shortcuts import render, redirect
 
+from .models import User
+
 
 def index(request):
-    # return redirect('/dashboard')
+    if "uuid" in request.session:
+        return redirect('/dashboard')
+
     return render(request, 'index.html')
 
 
 def register(request):
+    print(request.POST['first_name'])
+    errors = User.objects.registration_validator(request.POST)
+    print("************", errors)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+
+        return redirect("/")
+    else:
+        account = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+        new_user = User.objects.create(
+            first_name = request.POST['first_name'],
+            last_name = request.POST['last_name'],
+            email = request.POST['email'],
+            password = account
+        )
+
+        request.session['uuid'] = new_user.id
     return redirect('/dashboard')
 
 
 def login(request):
+    errors = User.objects.login_validator(request.POST)
+
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+
+        return redirect("/")
+    else:
+        user = User.objects.get(email = request.POST['email'])
+
+        request.session['uuid'] = user.id
+
     return redirect('/dashboard')
 
 
 def logout(request):
+    del request.session['uuid']
     return redirect('/')
 
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    if "uuid" not in request.session:
+        return redirect("/")
+    context = {
+        "logged_in_user": User.objects.get(id = request.session['uuid']),
+        "all_users": User.objects.all()
+    }
+
+    return render(request, "dashboard.html", context)
 
 # Create your views here.

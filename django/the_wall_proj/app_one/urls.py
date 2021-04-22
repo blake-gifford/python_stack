0bb75a138cd8e363fb97dc892b217ddb7c237b23
blake-gifford <blakegifford01@gmail.com>
2021-04-21 20:13:53 -0700

    night project commit

--- django/the_wall_proj/app_one/urls.py
+++ django/the_wall_proj/app_one/urls.py
@@ -5,7 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('login', views.login),
-    path('registration', views.registration),
+    path('register', views.register),
     path('logout', views.logout),
     path('homepage', views.homepage),
+    # display paths
+
+    # action paths
+    
 ]
--- django/the_wall_proj/app_one/views.py
+++ django/the_wall_proj/app_one/views.py
@@ -1,7 +1,64 @@
+import bcrypt
+from django.contrib import messages
 from django.shortcuts import render, redirect
 
+from .models import User
 
 def index(request):
+    if "uuid" in request.session:
+        return redirect('/homepage')
+
     return render(request, 'index.html')
 
+def register(request):
+    errors = User.objects.registration_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+
+        return redirect("/")
+    else:
+        account = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+        new_user = User.objects.create(
+            first_name = request.POST['first_name'],
+            last_name = request.POST['last_name'],
+            email = request.POST['email'],
+            password = account
+        )
+        request.session['uuid'] = new_user.id
+
+        return redirect('/homepage')
+
+def login(request):
+    errors = User.objects.login_validator(request.POST)
+
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+
+        return redirect('/')
+    else:
+        user = User.objects.get(email = request.POST['email'])
+
+        request.session['uuid'] = user.id
+
+    return redirect('/homepage')
+
+
+
+def logout(request):
+    request.session.clear()
+
+    return redirect('/')
+
+
+def homepage(request):
+    if "uuid" not in request.session:
+        return redirect("/")
+    context = {
+        "logged_in_user": User.objects.get(id = request.session['uuid']),
+        "all_users": User.objects.all()
+    }
+
+    return render(request, 'homepage.html')
 # Create your views here.
